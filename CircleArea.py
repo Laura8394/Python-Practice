26d7db172d352c88c74a2fa76a24c87b4e58cda5
Laura8394 <277611453@qq.com>
2018-05-17 18:35:47 +0800

    Change the file

--- CircleArea.py
+++ CircleArea.py
@@ -11,3 +11,7 @@ s = math.pi * r ** 2
 f = math.pi * r * 2
 print("The CircleFerence Is: {: .10f}".format(f))
 print("The CircleArea Is: {: .10f}".format(s))
+
+a = float(input("Enter the length: "))
+l = a * 4
+print("The Square CircumFerence Is {:5.2f}m".format(l))
