2ef75bf925add2eb2923c2f31b9d639618997de7
Laura8394 <277611453@qq.com>
2018-05-18 14:25:10 +0800

    "Create powerseries"

---
+++ powerseries.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 18 13:56:29 2018
+
+@author: laura
+"""
+#计算幂级数：e ** x = 1 + (x + x ** 2!) + x ** 3 / 3! + ... + x ** n / n! (0 < x < 1)
+
+x = float(input("Enter the value of x: "))
+n = term = num = 1
+result = 1.0
+while n <= 100:
+    term *= x / n
+    result += term
+    n += 1
+    if term < 0.0001:
+        break
+print("No of Times= {} and Sum= {}".format(n,result))
